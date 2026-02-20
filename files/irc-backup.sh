bde5c58bc21c4f06159ac4419d6f5ac2d92cae76
Gavin McConnell <gavinwmcconnell@gmail.com>
2026-02-20 11:51:13 -0500

    further fixing and restructuring to closer match how the service worked in the competition

--- files/irc-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple backup script - contains credentials
-# VULNERABILITY: Hardcoded credentials
-tar -czf /tmp/irc-backup-$(date +%Y%m%d).tar.gz {{ irc_log_dir }}
-# Upload to backup server (not configured)
-# BACKUP_USER=admin
-# BACKUP_PASS=backup123
-# IRC_PASSWORD=cyberpsychosis
